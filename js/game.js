4598790a06b356d0b30d8e921d911c411d61f849
viviancarvalho123 <vivian.carvalho2@estudante.ifms.edu.br>
2024-03-04 15:40:58 -0300

    ultima alteracao

--- js/game.js
+++ js/game.js
@@ -25,7 +25,7 @@ let secondCard = '';
 const checkEndGame = () => {
   const disabledCards = document.querySelectorAll('.disabled-card');
 
-  if (disabledCards.length === 2) {
+  if (disabledCards.length === 12) {
     clearInterval(this.loop);
     // alert(`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi de: ${timer.innerHTML}`);
     //document.write(`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi de: ${timer.innerHTML}`)
